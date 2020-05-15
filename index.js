7211d1cd3161d4260d9dc5ddeed386e22a471753
Cagin Agirdemir <caginagirdemir@gmail.com>
2020-05-16 02:31:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,83 @@
-const map = new harp.MapView({
-    canvas: document.getElementById("map"),
-    theme:
-        "https://unpkg.com/@here/harp-map-theme@latest/resources/berlin_tilezen_night_reduced.json"
-});
-const controls = new harp.MapControls(map);
-const omvDataSource = new harp.OmvDataSource({
-    baseUrl: "https://vector.hereapi.com/v2/vectortiles/base/mc",
-    apiFormat: harp.APIFormat.XYZOMV,
-    styleSetName: "tilezen",
-    authenticationCode: "APFoVhTuQQecn3TG5mYaEgA",
-    authenticationMethod: {
-        method: harp.AuthenticationMethod.QueryString,
-        name: "apikey"
+
+
+
+/*
+ * Copyright (C) 2017-2020 HERE Europe B.V.
+ * Licensed under Apache 2.0, see full license in LICENSE
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { GeoCoordinates } from "@here/harp-geoutils";
+import { MapControls, MapControlsUI } from "@here/harp-map-controls";
+import { CopyrightElementHandler, MapView, ThemeLoader } from "@here/harp-mapview";
+import { APIFormat, AuthenticationMethod, OmvDataSource } from "@here/harp-omv-datasource";
+import { GUI } from "dat.gui";
+import { apikey, copyrightInfo } from "../config";
+
+/**
+ * This example copies the base example and adds a GUI allowing to switch between all the open-
+ * sourced themes available in the repository.
+ */
+export namespace ThemesExample {
+    function initializeMapView(): MapView {
+        const canvas = document.getElementById("mapCanvas") as HTMLCanvasElement;
+        const map = new MapView({
+            canvas,
+            theme: "resources/berlin_tilezen_base.json"
+        });
+
+        CopyrightElementHandler.install("copyrightNotice", map);
+
+        const mapControls = new MapControls(map);
+        mapControls.maxTiltAngle = 50;
+
+        const moscow = new GeoCoordinates(55.7525631, 37.6234006);
+        map.lookAt({ target: moscow, zoomLevel: 16.1, tilt: 50, heading: 300 });
+        map.zoomLevel = 16.1;
+
+        const ui = new MapControlsUI(mapControls, { zoomLevel: "input", projectionSwitch: true });
+        canvas.parentElement!.appendChild(ui.domElement);
+
+        map.resize(window.innerWidth, window.innerHeight);
+
+        window.addEventListener("resize", () => {
+            map.resize(window.innerWidth, window.innerHeight);
+        });
+
+        return map;
     }
-});
-map.addDataSource(omvDataSource);
+
+    const mapView = initializeMapView();
+
+    const omvDataSource = new OmvDataSource({
+        baseUrl: "https://vector.hereapi.com/v2/vectortiles/base/mc",
+        apiFormat: APIFormat.XYZOMV,
+        styleSetName: "tilezen",
+        authenticationCode: "APFoVhTuQQecn3TG5mYaEgA",
+        authenticationMethod: {
+            method: AuthenticationMethod.QueryString,
+            name: "apikey"
+        },
+        copyrightInfo
+    });
+
+    mapView.addDataSource(omvDataSource);
+
+    const gui = new GUI({ width: 300 });
+    const options = {
+        theme: {
+            day: "resources/berlin_tilezen_base.json",
+            reducedDay: "resources/berlin_tilezen_day_reduced.json",
+            reducedNight: "resources/berlin_tilezen_night_reduced.json",
+            streets: "resources/berlin_tilezen_effects_streets.json",
+            outlines: "resources/berlin_tilezen_effects_outlines.json"
+        }
+    };
+    gui.add(options, "theme", options.theme)
+        .onChange((value: string) => {
+            ThemeLoader.load(value).then(theme => {
+                mapView.theme = theme;
+            });
+        })
+        .setValue("resources/berlin_tilezen_base.json");
+}
