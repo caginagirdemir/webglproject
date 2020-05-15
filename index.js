278dabcfa98706631a9f2da93c6108e8ba90068e
Cagin Agirdemir <caginagirdemir@gmail.com>
2020-05-16 02:29:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,17 @@
-const canvas = document.getElementById('map');
 const map = new harp.MapView({
-   canvas,
-   theme: "https://unpkg.com/@here/harp-map-theme@latest/resources/berlin_tilezen_night_reduced.json",
-   //For tile cache optimization:
-   maxVisibleDataSourceTiles: 40, 
-   tileCacheSize: 100
+    canvas: document.getElementById("map"),
+    theme:
+        "https://unpkg.com/@here/harp-map-theme@latest/resources/berlin_tilezen_night_reduced.json"
 });
-
-map.setCameraGeolocationAndZoom(
-   new harp.GeoCoordinates(1.278676, 103.850216),
-   16
-);
-
-const mapControls = new harp.MapControls(map);
-const ui = new harp.MapControlsUI(mapControls);
-canvas.parentElement.appendChild(ui.domElement);
-
-mapControls.maxPitchAngle = 90;
-mapControls.setRotation(6.3, 50);
-
-map.resize(window.innerWidth, window.innerHeight);
-window.onresize = () => map.resize(window.innerWidth, window.innerHeight);
-
+const controls = new harp.MapControls(map);
 const omvDataSource = new harp.OmvDataSource({
-   baseUrl: "https://xyz.api.here.com/tiles/herebase.02",
-   apiFormat: harp.APIFormat.XYZOMV,
-   styleSetName: "tilezen",
-   authenticationCode: 'APFoVhTuQQecn3TG5mYaEgA',
+    baseUrl: "https://vector.hereapi.com/v2/vectortiles/base/mc",
+    apiFormat: harp.APIFormat.XYZOMV,
+    styleSetName: "tilezen",
+    authenticationCode: "APFoVhTuQQecn3TG5mYaEgA",
+    authenticationMethod: {
+        method: harp.AuthenticationMethod.QueryString,
+        name: "apikey"
+    }
 });
 map.addDataSource(omvDataSource);
