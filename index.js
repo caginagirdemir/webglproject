9c1dbb45ef7f8a2902297d571ca4d4fd1b7d9d6b
Cagin Agirdemir <caginagirdemir@gmail.com>
2020-05-16 02:44:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,71 +1,31 @@
-export namespace HelloWorldExample {
-    // Create a new MapView for the HTMLCanvasElement of the given id.
-    function initializeMapView(id: string): MapView {
-        // snippet:harp_gl_hello_world_example_0.ts
-        const canvas = document.getElementById(id) as HTMLCanvasElement;
-        // end:harp_gl_hello_world_example_0.ts
+const canvas = document.getElementById('map');
+const map = new harp.MapView({
+   canvas,
+   theme: "https://unpkg.com/@here/harp-map-theme@latest/resources/berlin_tilezen_night_reduced.json",
+   //For tile cache optimization:
+   maxVisibleDataSourceTiles: 40, 
+   tileCacheSize: 100
+});
 
-        // snippet:harp_gl_hello_world_example_1.ts
-        // Look at New York.
-        const NY = new GeoCoordinates(40.707, -74.01);
-        const map = new MapView({
-            canvas,
-            theme: "resources/berlin_tilezen_base.json",
-            target: NY,
-            tilt: 50,
-            heading: -20,
-            zoomLevel: 16.1
-        });
-        // end:harp_gl_hello_world_example_1.ts
+map.setCameraGeolocationAndZoom(
+   new harp.GeoCoordinates(1.278676, 103.850216),
+   16
+);
 
-        CopyrightElementHandler.install("copyrightNotice", map);
+const mapControls = new harp.MapControls(map);
+const ui = new harp.MapControlsUI(mapControls);
+canvas.parentElement.appendChild(ui.domElement);
 
-        // snippet:harp_gl_hello_world_example_map_controls.ts
-        // Instantiate the default map controls, allowing the user to pan around freely.
-        const mapControls = new MapControls(map);
-        mapControls.maxTiltAngle = 50;
-        // end:harp_gl_hello_world_example_map_controls.ts
+mapControls.maxPitchAngle = 90;
+mapControls.setRotation(6.3, 50);
 
-        // Add an UI.
-        const ui = new MapControlsUI(mapControls, { zoomLevel: "input" });
-        canvas.parentElement!.appendChild(ui.domElement);
+map.resize(window.innerWidth, window.innerHeight);
+window.onresize = () => map.resize(window.innerWidth, window.innerHeight);
 
-        // snippet:harp_gl_hello_world_example_3.ts
-        // Resize the mapView to maximum.
-        map.resize(window.innerWidth, window.innerHeight);
-
-        // React on resize events.
-        window.addEventListener("resize", () => {
-            map.resize(window.innerWidth, window.innerHeight);
-        });
-        // end:harp_gl_hello_world_example_3.ts
-
-        addOmvDataSource(map);
-
-        return map;
-    }
-
-    function addOmvDataSource(map: MapView) {
-        // snippet:harp_gl_hello_world_example_4.ts
-        const omvDataSource = new OmvDataSource({
-            baseUrl: "https://vector.hereapi.com/v2/vectortiles/base/mc",
-            apiFormat: APIFormat.XYZOMV,
-            styleSetName: "tilezen",
-            authenticationCode: "Ad4fCHddQkypCAf0qmFrAAA",
-            authenticationMethod: {
-                method: AuthenticationMethod.QueryString,
-                name: "apikey"
-            },
-            copyrightInfo
-        });
-        // end:harp_gl_hello_world_example_4.ts
-
-        // snippet:harp_gl_hello_world_example_5.ts
-        map.addDataSource(omvDataSource);
-        // end:harp_gl_hello_world_example_5.ts
-
-        return map;
-    }
-
-    export const mapView = initializeMapView("mapCanvas");
-}
+const omvDataSource = new harp.OmvDataSource({
+   baseUrl: "https://xyz.api.here.com/tiles/herebase.02",
+   apiFormat: harp.APIFormat.XYZOMV,
+   styleSetName: "tilezen",
+   authenticationCode: 'Ad4fCHddQkypCAf0qmFrAAA',
+});
+map.addDataSource(omvDataSource);
