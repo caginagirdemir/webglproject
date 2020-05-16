8f2af2a19e553b5f90c3dc015037132e783ed927
Cagin Agirdemir <caginagirdemir@gmail.com>
2020-05-16 03:19:29 +0300

    Delete webpack.config.js

--- webpack.config.js
+++
@@ -1,6 +0,0 @@
-const { addHarpWebpackConfig } = require("@here/harp-webpack-utils/scripts/HarpWebpackConfig");
-const myConfig = {};
-module.exports = addHarpWebpackConfig(
-    myConfig,
-    { mainEntry: "./index.js", decoderEntry: "./harp-gl-decoders.js", htmlTemplate: "./index.html" }
-);
