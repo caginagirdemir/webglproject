4f16243d6bcd57783d05ff56dcffceb83349bf30
Cagin Agirdemir <caginagirdemir@gmail.com>
2020-05-24 00:32:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const canvas = document.getElementById('map');
 const map = new harp.MapView({
    canvas: canvas,
-   theme: "https://unpkg.com/@here/harp-map-theme@latest/resources/berlin_tilezen_night_reduced.json",
+   theme: "https://unpkg.com/@here/harp-map-theme@latest/resources/berlin_tilezen_base.json",
+   projection: harp.sphereProjection,
    //For tile cache optimization:
    maxVisibleDataSourceTiles: 40, 
    tileCacheSize: 100
 });
 
 map.setCameraGeolocationAndZoom(
-   new harp.GeoCoordinates(1.278676, 103.850216), //latitude, longitude
-   16 //zoom level 
+   new harp.GeoCoordinates(39.933365, 32.859741), //latitude, longitude
+   4 //zoom level 
 );
 
-const mapControls = new harp.MapControls(map);
-const ui = new harp.MapControlsUI(mapControls);
-canvas.parentElement.appendChild(ui.domElement);
+// const mapControls = new harp.MapControls(map);
+// const ui = new harp.MapControlsUI(mapControls);
+// canvas.parentElement.appendChild(ui.domElement);
 
-mapControls.maxPitchAngle = 90;
-//mapControls.setRotation(6.3, 50);
+// mapControls.maxPitchAngle = 40;
+// //mapControls.setRotation(6.3, 50);
+
+const controls = new GlobeControls(map);
+controls.enabled = true;
 
 map.resize(window.innerWidth, window.innerHeight);
 window.onresize = () => map.resize(window.innerWidth, window.innerHeight);
